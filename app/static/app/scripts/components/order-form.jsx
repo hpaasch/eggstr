9a06d595e63f6aec16c132647c732933b51bc880
Dan Dietz <danddietz@gmail.com>
2016-07-14 11:30:46 -0400

    hooking up end points

--- app/static/app/scripts/components/order-form.jsx
+++ app/static/app/scripts/components/order-form.jsx
@@ -2,23 +2,49 @@ var React = require('react');
 
 var models = require('../models/eggs');
 
+
 var OrderForm = React.createClass({
   getInitialState: function(){
-    return {'eggOptions': []};
+    var orders = new models.OrderCollection();
+
+    return {
+      'eggOptions': [],
+      'option': '',
+      'name': '',
+      'orders': orders
+    };
   },
   componentWillMount: function(){
+    var self = this;
     var eggOptions = new models.EggOptionCollection();
 
+    //orders.fetch().done(function());
+
     eggOptions.fetch().done(function(){
-      this.setState({eggOptions: eggOptions});
+      self.setState({eggOptions: eggOptions});
     });
   },
+  handleSubmit: function(e){
+    e.preventDefault();
+    console.log(this.state);
+    this.state.orders.create({
+      'name': this.state.name,
+      'egg_option': this.state.option
+    });
+  },
+  handleNameChange: function(e){
+    this.setState({'name': e.target.value});
+  },
+  handleEggOptionChange: function(e){
+    this.setState({'option': e.target.value});
+  },
   render: function(){
+    var self = this;
     var eggOptionList = this.state.eggOptions.map(function(model){
       return (
         <li key={model.get('id')}>
           <img src={model.get('image_url')} /> <br/>
-          <input type="radio" name="option" value={model.get('id')} />
+          <input onChange={self.handleEggOptionChange} type="radio" name="option" value={model.get('id')} />
         </li>
       );
     });
@@ -27,17 +53,19 @@ var OrderForm = React.createClass({
       <div className="container">
         <div className="row">
           <div className="md-col-6">
-            <form>
+            <form onSubmit={this.handleSubmit}>
               <h1>Please Order Your Awesome Egg</h1>
 
               <div className="form-group">
                 <label htmlFor="name">Name</label>
-                <input type="text" className="form-control" id="name" placeholder="Sir/Madam Your Name Please" />
+                <input onChange={this.handleNameChange} value={this.state.name} type="text" className="form-control" id="name" placeholder="Sir/Madam Your Name Please" />
               </div>
 
               <ul>
                 {eggOptionList}
               </ul>
+
+              <input className="btn btn-danger" type="submit" value="Place Your Order!" />
             </form>
           </div>
         </div>
