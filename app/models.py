8a15d7e16e3e15c77602c680dccb4581465fded5
taddeimania <jtaddei@gmail.com>
2016-07-14 10:20:01 -0400

    JOEL: Started database structure

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class EggOption(models.Model):
+    image_url = models.URLField()
+    name = models.CharField(max_length=30)
+
+
+class Order(models.Model):
+    name = models.CharField(max_length=50)
+    egg_option = models.ForeignKey(EggOption)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ("created",)
